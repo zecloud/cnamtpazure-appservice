3860b359cef3118be7fc447607cc49a76f6e1413
Aymeric Weinbach <aymeric@zecloud.fr>
2020-04-15 09:30:02 +0200

    first import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FilePond } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
 
 function App() {
   return (
@@ -16,9 +18,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by Aymeric
         </a>
       </header>
+      <FilePond server="https://cnamtpfuncayw.azurewebsites.net/api/UploadPhoto?code=3EuwYbV4M0VmzwQXY4wSkbchkWtmRsd0bhX/7PHfoySM2D2RZwpmDQ=="/>
     </div>
   );
 }
